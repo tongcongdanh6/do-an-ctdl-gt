bf4b889deb45edd3629d22b97b26019f14c295de
unknown <tong.congdanh@gmail.com>
2020-10-19 00:07:14 +0700

    add 19102020 1207

--- DoAn_CTDLGT_1988216/DoAn_CTDLGT_1988216/Program.cs
+++ DoAn_CTDLGT_1988216/DoAn_CTDLGT_1988216/Program.cs
@@ -34,10 +34,65 @@ namespace DoAn_CTDLGT_1988216
 
             return intArr;
         }
+
+        static void WriteHexNumbers(int[] intArr)
+        {
+            string hexString = "";
+
+            for(int i = 0; i < intArr.Length; i++)
+            {
+                if(i < intArr.Length - 1)
+                {
+                    hexString += intArr[i].ToString("X") + " ";
+                }
+                else
+                {
+                    hexString += intArr[i].ToString("X");
+                }
+            }
+           
+            StreamWriter file = new StreamWriter("../../ex1data_output.txt");
+            file.Write(hexString);
+            file.Close();
+
+            Console.Write("File OUTPUT Wrote!!!");
+        }
+
+        static int[] SelectionSort(int[] intArr)
+        {
+            for(int i = 0; i < intArr.Length; i++)
+            {
+                int minIdx = i;
+                // Tìm min trong array
+                for (int j = i + 1; j < intArr.Length; j++)
+                {
+                    if(intArr[j] < intArr[minIdx])
+                    {
+                        minIdx = j;
+                    }
+                }
+                // Swap min với vị trí thứ i hiện tại
+                int tmp = intArr[i];
+                intArr[i] = intArr[minIdx];
+                intArr[minIdx] = tmp;
+            }
+
+            return intArr;
+        }
+
         static void Main(string[] args)
         {
             int[] arrInt = ReadHexNumbers("../../ex1data.txt");
+            int[] selectionshort = SelectionSort(arrInt);
+            Console.Write("\nSelection Short:\n");
+            foreach (int a in selectionshort)
+            {
+                Console.WriteLine(a);
+            }
 
+
+            Console.Write("\n\nWRITE TO FILE:\n\n");
+            WriteHexNumbers(selectionshort);
             Console.ReadLine();
         }
 
