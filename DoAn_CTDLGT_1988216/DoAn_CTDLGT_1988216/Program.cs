9e06d5a2d4744ec732fde004858e0d518c8bfb19
unknown <tong.congdanh@gmail.com>
2020-10-19 13:37:27 +0700

    19102020 1337

--- DoAn_CTDLGT_1988216/DoAn_CTDLGT_1988216/Program.cs
+++ DoAn_CTDLGT_1988216/DoAn_CTDLGT_1988216/Program.cs
@@ -14,25 +14,21 @@ namespace DoAn_CTDLGT_1988216
         {
             StreamReader file = new StreamReader(FilePath);
             string ln;
-            ln = file.ReadLine();
+            List<int> stringList = new List<int>();
+
+            while((ln = file.ReadLine()) != null)
+            {
+                string[] strArr = ln.Split(' '); // Tách chuỗi để đưa vào array bởi space
+                int numOfElement = strArr.Length;
+
+                for (int i = 0; i < numOfElement; i++)
+                {
+                    stringList.Add(Convert.ToInt32(strArr[i], 16));
+                }
+            }
             file.Close();
 
-            string[] strArr = ln.Split(' '); // Tách chuỗi để đưa vào array bởi space
-            int numOfElement = strArr.Length;
-            int[] intArr = new int[numOfElement]; // Khởi tạo mảng số nguyên
-
-            for(int i = 0; i < numOfElement; i++)
-            {
-                intArr[i] = Convert.ToInt32(strArr[i], 16);
-            }
-
-            // For DEBUG
-            for (int i = 0; i < numOfElement; i++)
-            {
-                Console.Write(intArr[i]+" ");
-            }
-
-            return intArr;
+            return stringList.ToArray(); // Chuyển đổi từ List qua Array
         }
 
         static void WriteHexNumbers(int[] intArr)
@@ -43,7 +39,14 @@ namespace DoAn_CTDLGT_1988216
             {
                 if(i < intArr.Length - 1)
                 {
-                    hexString += intArr[i].ToString("X") + " ";
+                    if(intArr[i] >= 0 && intArr[i] < 16)
+                    {
+                        hexString += "0" + intArr[i].ToString("X") + " ";
+                    }
+                    else
+                    {
+                        hexString += intArr[i].ToString("X") + " ";
+                    }
                 }
                 else
                 {
@@ -80,19 +83,126 @@ namespace DoAn_CTDLGT_1988216
             return intArr;
         }
 
+        static public int BinarySearch(int[] arr, int left, int right, int x)
+        {
+            // Nếu chưa hết mảng thì thực hiện Binary Search
+            if(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (x < arr[mid]) // x < a[mid] tìm phần bên trái bằng cách hiệu chỉnh lại right
+                {
+                    return BinarySearch(arr, left, mid - 1, x);
+                }
+                if (x > arr[mid]) // x > a[mid] tìm phần bên phải bằng cách hiệu chỉnh lại left
+                {
+                    return BinarySearch(arr, mid + 1, right, x);
+                }
+                else // Trường hợp x = a[mid] thì trả về index = mid;
+                {
+                    return mid;
+                }
+            }
+            // Nếu hết mảng rồi mà vẫn chưa tìm được x = a[mid] thì return -1
+            return -1;
+        }
+
+        static public Queue getOdd(int[] arr)
+        {
+            Queue oddQueue = new Queue();
+            foreach(int x in arr)
+            {
+                if(x % 2 != 0)
+                {
+                    oddQueue.push(x);
+                }
+            }
+            return oddQueue;
+        }
+
+        static public Queue getEven(int[] arr)
+        {
+            Queue evenQueue = new Queue();
+            foreach (int x in arr)
+            {
+                if (x % 2 == 0)
+                {
+                    evenQueue.push(x);
+                }
+            }
+            return evenQueue;
+        }
+
+        static public void writeToFile(Queue myQueue, string fileName)
+        {
+            string hexString = "";
+            int counter = myQueue.Count(); // Số lượng phần tử hiện có trong hàng đợi
+
+            for (int i = 0; i < counter; i++)
+            {
+                if (i < counter - 1)
+                {
+                    if (myQueue.getFromQueue(i) >= 0 && myQueue.getFromQueue(i) < 16)
+                    {
+                        hexString += "0" + myQueue.getFromQueue(i).ToString("X") + " ";
+                    }
+                    else
+                    {
+                        hexString += myQueue.getFromQueue(i).ToString("X") + " ";
+                    }
+                }
+                else
+                {
+                    hexString += myQueue.getFromQueue(i).ToString("X");
+                }
+            }
+
+            StreamWriter file = new StreamWriter("../../"+ fileName+".txt");
+            file.Write(hexString);
+            file.Close();
+
+            Console.Write("File " + fileName + ".txt Wrote!!!");
+        }
+
         static void Main(string[] args)
         {
-            int[] arrInt = ReadHexNumbers("../../ex1data.txt");
-            int[] selectionshort = SelectionSort(arrInt);
+            int[] arrInt = ReadHexNumbers("../../hex_numbers.txt");
+
+            Console.Write("\n\n ARRAY FROM ORIGINAL FILE:\n\n");
+            foreach(int x in arrInt)
+            {
+                Console.Write(x + " ");
+            }
+
+            int[] SortedArrayBySelectionSort = SelectionSort(arrInt);
             Console.Write("\nSelection Short:\n");
-            foreach (int a in selectionshort)
+            foreach (int a in SortedArrayBySelectionSort)
             {
                 Console.WriteLine(a);
             }
 
 
             Console.Write("\n\nWRITE TO FILE:\n\n");
-            WriteHexNumbers(selectionshort);
+            WriteHexNumbers(SortedArrayBySelectionSort);
+            
+
+            Console.Write("\n\n BINARY SEARCH WITH x = 255:\n\n");
+            Console.Write(BinarySearch(SortedArrayBySelectionSort, 0, SortedArrayBySelectionSort.Length - 1, 255));
+
+            Console.Write("\n\n BINARY SEARCH WITH x = 254:\n\n");
+            Console.Write(BinarySearch(SortedArrayBySelectionSort, 0, SortedArrayBySelectionSort.Length - 1, 254));
+
+
+            Console.Write("\n\n QUEUE:\n\n");
+            Queue oddQueue = getOdd(SortedArrayBySelectionSort);
+            Queue evenQueue = getEven(SortedArrayBySelectionSort);
+            Console.Write("\n\n ODD QUEUE:\n\n");
+            oddQueue.printQueue();
+            writeToFile(oddQueue, "odd_queue");
+            Console.Write("\n\n EVEN QUEUE:\n\n");
+            evenQueue.printQueue();
+            writeToFile(evenQueue, "even_queue");
+
             Console.ReadLine();
         }
 
---
+++ DoAn_CTDLGT_1988216/DoAn_CTDLGT_1988216/Queue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_CTDLGT_1988216
+{
+    class Queue
+    {
+        private LinkedList<int> list;
+
+        public Queue()
+        {
+            list = new LinkedList<int>();
+        }
+
+        public void IsQueueFull()
+        {
+
+            Console.Write(list.Count);
+        }
+
+        public int Count()
+        {
+            return list.Count;
+        }
+
+        public int getFromQueue(int pos)
+        {
+            return list.ElementAt<int>(pos);
+        }
+
+        public void push(int x)
+        {
+            // Đưa phần tử x vào cuối hàng đợi
+            list.AddLast(x);
+        }
+
+        public void pop()
+        {
+            // Lấy phần tử đầu hàng đợi
+            list.RemoveFirst();
+        }
+
+        public void printQueue()
+        {
+            foreach(int x in list)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
